3832d420c2605813903607c3af46d3a7ced158ba
Rafael <2001campos.rafael@gmail.com>
2024-02-20 21:50:09 -0300

    aula 20/02

---
+++ AULA03/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.Sqlite;
+
+// var conexao =  new SqliteConnection("Data Souce=db/dados.db");
+
+// conexao.Open();
+
+// conexao.Close();
+
+
+//outro método de fazer a conexão
+using Microsoft.Data.Sqlite;
+
+using(var conexao =  new SqliteConnection("Data Souce=db/dados.db");)
+{
+conexao.Open();
+}
