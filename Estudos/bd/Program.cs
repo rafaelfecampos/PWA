6fb2ce106f41fd8cbc1160febcd291db33231832
Rafael Ferreira Campos <2001campos.rafael@gmail.com>
2024-03-12 10:56:29 -0300

    commit 12/03

---
+++ Estudos/bd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Data.Sqlite;
+using objeto;
+
+Obj obj = new Obj();
+
+Console.WriteLine("Informe as informações do cliente a ser adicionado no banco de dados: ");
+Console.Write("-Id: ");
+obj.Id = Console.ReadLine();
+Console.Write("-Nome: ");
+obj.Nome = Console.ReadLine();
+Console.Write("-Email: ");
+obj.Email = Console.ReadLine();
+Console.Write("-Telefone: ");
+obj.Telefone = Console.ReadLine();
+
+
+using(var connection = new SqliteConnection("Data Source = database/db.db")){
+    connection.Open();
+
+    var command = connection.CreateCommand();
+
+    command.CommandText = "INSERT INTO cliente(id, nome, email, telefone)"+
+                    " VALUES (@Id, @Nome, @Email, @Telefone)";
+    
+    command.Parameters.AddWithValue("@Id", obj.Id);
+    command.Parameters.AddWithValue("@Nome", obj.Nome);
+    command.Parameters.AddWithValue("@Email", obj.Email);
+    command.Parameters.AddWithValue("@Telefone", obj.Telefone);
+
+    command.ExecuteNonQuery();
+
+
+    connection.Close();
+}
